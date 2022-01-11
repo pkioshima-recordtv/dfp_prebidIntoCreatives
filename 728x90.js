05a7ba8055fbdebce1465995d50f999a07007f54
pkioshima-recordtv <95826765+pkioshima-recordtv@users.noreply.github.com>
2022-01-11 11:00:23 -0300

    Create 728x90.js

---
+++ 728x90.js
@@ -0,0 +1,72 @@
+var sizes = [
+        [728, 90]
+        ];
+        var PREBID_TIMEOUT = 2300;            
+        var adUnits = [{
+            code: 'postbid_iframe',
+            mediaTypes: {
+                banner: {
+                    sizes: sizes
+                }
+            },
+            bids: [
+            {
+              bidder: 'appnexus',
+              params: {
+                placementId: "23560609"
+            }
+        },{
+              bidder: 'smartadserver',
+              params: {
+                siteId: "297790",
+                pageId: "1495461",
+                formatId: "78785"
+            }
+        },{
+              bidder: 'rubicon',
+              params: {
+                accountId: "15284",
+                siteId: "404604",
+                zoneId: "2266404"
+            }
+        }
+        ]
+}];
+
+var pbjs = pbjs || {};
+pbjs.que = pbjs.que || [];
+
+pbjs.que.push(function() {
+    pbjs.addAdUnits(adUnits);
+    pbjs.requestBids({
+        timeout: PREBID_TIMEOUT,
+        bidsBackHandler: function() {
+            var iframe = document.getElementById('postbid_iframe');
+            var iframeDoc = iframe.contentWindow.document;
+            var adServerTargeting = pbjs.getAdserverTargetingForAdUnitCode('postbid_iframe');
+
+                        // If any bidders return any creatives
+                        if (adServerTargeting && adServerTargeting['hb_adid']) {
+                            pbjs.renderAd(iframeDoc, adServerTargeting['hb_adid']);
+                        } else {
+                            iframe.width = sizes[0][0];
+                            iframe.height = sizes[0][1];
+                            iframeDoc.write('<head></head><body>' + passbackTagHtml + '</body>');
+                            iframeDoc.close();
+                        }
+                    }
+                });
+});     
+
+var passbackTagHtml="";
+passbackTagHtml += "<script async src=\"https:\/\/securepubads.g.doubleclick.net\/tag\/js\/gpt.js\"><\/script>";
+passbackTagHtml += "<div id=\"gpt-passback\">";
+passbackTagHtml += "<script>";
+passbackTagHtml += "window.googletag = window.googletag || {cmd: []};";
+passbackTagHtml += "googletag.cmd.push(function() {";
+passbackTagHtml += "googletag.defineSlot('\/7542\/TESTE\/prebid_passback', [[728, 90]], 'gpt-passback').addService(googletag.pubads());";
+passbackTagHtml += "googletag.enableServices();";
+passbackTagHtml += "googletag.display('gpt-passback');";
+passbackTagHtml += "});";
+passbackTagHtml += "<\/script>";
+passbackTagHtml += "<\/div>";
