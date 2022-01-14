0e62ea521e0eee82569f1a1c6a2592673e5ee564
pkioshima-recordtv <95826765+pkioshima-recordtv@users.noreply.github.com>
2022-01-14 14:38:16 -0300

    Update 300x600.js

--- 300x600.js
+++ 300x600.js
@@ -65,6 +65,7 @@ passbackTagHtml += "<script>";
 passbackTagHtml += "window.googletag = window.googletag || {cmd: []};";
 passbackTagHtml += "googletag.cmd.push(function() {";
 passbackTagHtml += "googletag.defineSlot('\/7542\/TESTE\/prebid_passback', [[300, 600]], 'gpt-passback').addService(googletag.pubads());";
+passbackTagHtml += "googletag.pubads().enableSingleRequest();";
 passbackTagHtml += "googletag.enableServices();";
 passbackTagHtml += "googletag.display('gpt-passback');";
 passbackTagHtml += "});";
