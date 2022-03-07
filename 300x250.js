4c83a0eaffe7274f57483550831e9693b22491a8
pkioshima-recordtv <95826765+pkioshima-recordtv@users.noreply.github.com>
2022-03-07 11:23:46 -0300

    Update 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -3,7 +3,7 @@ var sizes = [
         ];
         var PREBID_TIMEOUT = 2300;            
         var adUnits = [{
-            code: 'r7adunit',
+            code: 'postbid_iframe',
             mediaTypes: {
                 banner: {
                     sizes: sizes
@@ -61,3 +61,39 @@ userSync: {
     }]
 }
 });
+
+pbjs.que.push(function() {
+    pbjs.addAdUnits(adUnits);
+    pbjs.requestBids({
+        timeout: PREBID_TIMEOUT,
+        bidsBackHandler: function() {
+            var iframe = document.getElementById('postbid_iframe');
+            var iframeDoc = iframe.contentWindow.document;
+            var adServerTargeting = pbjs.getAdserverTargetingForAdUnitCode('postbid_iframe');
+
+                        // If any bidders return any creatives
+                        if (adServerTargeting && adServerTargeting['hb_adid']) {
+                            pbjs.renderAd(iframeDoc, adServerTargeting['hb_adid']);
+                        } else {
+                            iframe.width = sizes[0][0];
+                            iframe.height = sizes[0][1];
+                            iframeDoc.write('<head></head><body>' + passbackTagHtml + '</body>');
+                            iframeDoc.close();
+                        }
+                    }
+                });
+}); 
+
+var passbackTagHtml="";
+passbackTagHtml += "<script async src=\"https:\/\/securepubads.g.doubleclick.net\/tag\/js\/gpt.js\"><\/script>";
+passbackTagHtml += "<div id=\"gpt-passback\">";
+passbackTagHtml += "<script>";
+passbackTagHtml += "window.googletag = window.googletag || {cmd: []};";
+passbackTagHtml += "googletag.cmd.push(function() {";
+passbackTagHtml += "googletag.defineSlot('\/7542\/TESTE\/prebid_passback', [[300, 250]], 'gpt-passback').addService(googletag.pubads());";
+passbackTagHtml += "googletag.pubads().enableSingleRequest();";
+passbackTagHtml += "googletag.enableServices();";
+passbackTagHtml += "googletag.display('gpt-passback');";
+passbackTagHtml += "});";
+passbackTagHtml += "<\/script>";
+passbackTagHtml += "<\/div>";
