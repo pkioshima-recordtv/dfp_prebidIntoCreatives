df12673fbb8acbffbe136dd731bdcb348d3c7383
pkioshima-recordtv <95826765+pkioshima-recordtv@users.noreply.github.com>
2022-03-07 11:16:17 -0300

    Update 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -3,7 +3,7 @@ var sizes = [
         ];
         var PREBID_TIMEOUT = 2300;            
         var adUnits = [{
-            code: 'postbid_iframe',
+            code: 'r7adunit',
             mediaTypes: {
                 banner: {
                     sizes: sizes
@@ -22,7 +22,7 @@ var sizes = [
                 placementId: "23560609"
             }
         },{
-              bidder: 'onemobile',
+              bidder: 'yahoossp',
               params: {
                 dcn: "8a96905a017e7e78c12c7934ff0e0017",
                 pos: "8a969551017e7e78c5ff793602f0001a"
@@ -48,38 +48,16 @@ var sizes = [
 var pbjs = pbjs || {};
 pbjs.que = pbjs.que || [];
 
-pbjs.que.push(function() {
-    pbjs.addAdUnits(adUnits);
-    pbjs.requestBids({
-        timeout: PREBID_TIMEOUT,
-        bidsBackHandler: function() {
-            var iframe = document.getElementById('postbid_iframe');
-            var iframeDoc = iframe.contentWindow.document;
-            var adServerTargeting = pbjs.getAdserverTargetingForAdUnitCode('postbid_iframe');
-
-                        // If any bidders return any creatives
-                        if (adServerTargeting && adServerTargeting['hb_adid']) {
-                            pbjs.renderAd(iframeDoc, adServerTargeting['hb_adid']);
-                        } else {
-                            iframe.width = sizes[0][0];
-                            iframe.height = sizes[0][1];
-                            iframeDoc.write('<head></head><body>' + passbackTagHtml + '</body>');
-                            iframeDoc.close();
-                        }
-                    }
-                });
-}); 
-
-var passbackTagHtml="";
-passbackTagHtml += "<script async src=\"https:\/\/securepubads.g.doubleclick.net\/tag\/js\/gpt.js\"><\/script>";
-passbackTagHtml += "<div id=\"gpt-passback\">";
-passbackTagHtml += "<script>";
-passbackTagHtml += "window.googletag = window.googletag || {cmd: []};";
-passbackTagHtml += "googletag.cmd.push(function() {";
-passbackTagHtml += "googletag.defineSlot('\/7542\/TESTE\/prebid_passback', [[300, 250]], 'gpt-passback').addService(googletag.pubads());";
-passbackTagHtml += "googletag.pubads().enableSingleRequest();";
-passbackTagHtml += "googletag.enableServices();";
-passbackTagHtml += "googletag.display('gpt-passback');";
-passbackTagHtml += "});";
-passbackTagHtml += "<\/script>";
-passbackTagHtml += "<\/div>";
+pbjs.setConfig({
+userSync: {
+    filterSettings: {
+            iframe: {
+                bidders: '*',   // '*' means all bidders
+                filter: 'include'
+            }
+        },
+    userIds: [{
+        name: "criteo",
+    }]
+}
+});
