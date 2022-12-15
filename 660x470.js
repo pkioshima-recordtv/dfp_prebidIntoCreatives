36033867feab51a6f7f4c60b3459e6b2d23735a2
pkioshima-recordtv <95826765+pkioshima-recordtv@users.noreply.github.com>
2022-12-15 13:42:35 -0300

    Update 660x470.js

--- 660x470.js
+++ 660x470.js
@@ -36,7 +36,7 @@ var sizes = [
 var pbjs = pbjs || {};
 pbjs.que = pbjs.que || [];
 
-pbjs.setConfig({
+/*pbjs.setConfig({
 userSync: {
     filterSettings: {
             iframe: {
@@ -48,7 +48,7 @@ userSync: {
         name: "criteo",
     }]
 }
-});
+});*/
 
 pbjs.que.push(function() {
     pbjs.addAdUnits(adUnits);
